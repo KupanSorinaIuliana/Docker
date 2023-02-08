95a85e1de2f8f2d4c7aefb58946487136a50b648
KupanSorinaIuliana <sorinakupan3@gmail.com>
2023-02-08 21:30:59 +0200

    add: mongo db atlas file

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { join } from 'path';
 
 @Module({
   imports: [
-      MongooseModule.forRoot('mongodb://localhost/soa', { useNewUrlParser: true }),
+      MongooseModule.forRoot('mongodb+srv://sorina:%40N!r0%2410%2B1@bigdata.41058td.mongodb.net/test', { useNewUrlParser: true }),
       IdeaTrackerModule,
       ServeStaticModule.forRoot({
         rootPath: join(__dirname, '..','build'),
